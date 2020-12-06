132e41a6c6be05e513b8fe446fd3841c3617799e
mtyerdua <58461935+mtyerdua@users.noreply.github.com>
2020-12-06 17:58:18 +0100

    Bidirectionals relations with doctrine

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Episode;
 use App\Entity\Program;
+use App\Entity\Season;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -43,7 +45,44 @@ class ProgramController extends AbstractController
             );
         }
 
-        return $this->render('program/show.html.twig', ['program' => $program]);
+        $seasons = $this->getDoctrine()
+            ->getRepository(Season::class)
+            ->findBy(['program' => $program]);
+
+        return $this->render('program/show.html.twig', [
+            'program' => $program,
+            'seasons' => $seasons,
+            ]);
+
+    }
+    /**
+     * @Route("/{programId}/seasons/{seasonId}", methods={"GET"}, name="season_show")
+     */
+
+    public function showSeason(int $programId, int $seasonId): Response
+    {
+    $program = $this->getDoctrine()
+        ->getRepository(Program::class)
+        ->findOneBy(['id' => $programId]);
+
+
+    $season = $this->getDoctrine()
+        ->getRepository(Season::class)
+        ->findOneBy(['id' => $seasonId]);
+
+    $episodes = $this->getDoctrine()
+        ->getRepository(Episode::class)
+        ->findBy(['season' => $seasonId]);
+
+
+    return $this->render('program/season_show.html.twig', [
+        'program' => $program,
+        'season' => $season,
+        'episodes' => $episodes,
+
+
+    ]);
+
 
     }
 }
